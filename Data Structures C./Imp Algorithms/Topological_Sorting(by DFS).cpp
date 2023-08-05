56f23650716efefe1c4200b2918df1445bc5227b
Arjun R Nambiar <95574870+ShadowSlayer03@users.noreply.github.com>
2023-08-05 18:10:55 +0530

    Update Topological_Sorting(by DFS).cpp

--- Data Structures C./Imp Algorithms/Topological_Sorting(by DFS).cpp
+++ Data Structures C./Imp Algorithms/Topological_Sorting(by DFS).cpp
@@ -4,18 +4,17 @@
 #include<stack>
 using namespace std;
 
-void dfs(int node,stack<int>& st,vector<vector<pair<int,int>>> &adj,int vis[])
+void dfs(int node,stack<int>& st,vector<vector<int>> &adj,int vis[])
 {
   vis[node] = 1;
   for(auto it: adj[node])
   {
-    int adjacentNode = it.first;
-    if(vis[adjacentNode]==0)
-      dfs(adjacentNode,st,adj,vis);
+    if(vis[it]==0)
+      dfs(it,st,adj,vis);
   }
   st.push(node);
 }
-void topoSort(vector<vector<pair<int,int>>> &adj, int V)
+void topoSort(vector<vector<int>> &adj, int V)
 {
   int* vis = new int[V] {0};
   stack<int> S;
@@ -46,6 +45,7 @@ int main()
     cout<<"Enter the graph values in this format: (source destination)"<<endl;
     cin>>source>>destination;
     adj[source].push_back(destination);
+  }
 
   topoSort(adj,V);
   return 0;
