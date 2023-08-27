4ec294d6680bf82bb7a59c588b9de235a8750abc
Arjun R Nambiar <95574870+ShadowSlayer03@users.noreply.github.com>
2023-08-27 13:41:51 +0530

    Quick Sort

---
+++ Data Structures C./Sorting/SORTING_quick_sort.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+void swap(int* a, int* b) {
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = (low - 1);
+
+    for (int j = low; j <= high - 1; j++) {
+
+        if (arr[j] <= pivot) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+
+        int pi = partition(arr, low, high);
+
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+int main() {
+    int size;
+    cout << "Enter the number of elements: ";
+    cin >> size;
+
+    int arr[size];
+    cout << "Enter the elements:" << endl;
+    for (int i = 0; i < size; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Original array: ";
+    printArray(arr, size);
+    quickSort(arr, 0, size - 1);
+
+    cout << "Sorted array: ";
+    printArray(arr, size);
+
+    return 0;
+}
